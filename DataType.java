0cda7b856da9d8703596b3873a40e0104a1de856
santoshr1982 <santoshr1982@yahoo.com>
2016-03-20 13:15:02 +0530

    Added files via upload

---
+++ DataType.java
@@ -0,0 +1,11 @@
+package Data.Type;
+// This program has been written to mention the two data type that can not be stored in Int data type.
+public class DataType {								//Class Declaration.
+	
+	public static void main(String[] args) {		//Main Method.
+		
+		
+		System.out.println(" Char and String are two type of data that can not be stored in int type.");// Print the output.
+	}
+
+}
